7da30511abed544a4e8088a465dadfde6ab57f28
uilton <uilton@meta4chain.com>
2026-05-10 13:57:50 -0300

    chore: bootstrap Spring Boot service with initial project structure

---
+++ app/src/main/java/dev/br/platform/ClaudeAgenticPlatformApplication.java
@@ -0,0 +1,13 @@
+package dev.br.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeAgenticPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeAgenticPlatformApplication.class, args);
+	}
+
+}
